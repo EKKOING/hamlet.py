8e68db933f629b210e264403ed75465c34433c49
Nicholas Lorentzen <nicholas.lorentzen@gmail.com>
2020-04-27 01:27:19 -0400

    Create toBeOrNotToBe.py

---
+++ toBeOrNotToBe.py
@@ -0,0 +1,39 @@
+# toBeOrNotToBe.py
+
+from brain import thoughts
+
+question = thoughts.ponder("suicide")
+
+if suffering() < death():
+    self.sleep(int.max)
+
+def suffering():
+    self.suffering = 0
+    import random
+    for day in self.life:
+        self.suffering += random.uniform(a=0,b=100)
+    return self.suffering
+
+def death():
+    death = self.sleep()
+    self.heartache = None
+    self.shocks = None
+
+self.wish(for heartbreak=false, surprises=false)
+self.doubt()
+question.ponder()
+
+
+try:
+    self.kill()
+    if self.doesDream:
+        raise thoughts.DoesDreamError("Dreams may still occur in death")
+except thoughts.DoesDreamError:
+    self.live()
+
+from life import events
+if self.future.contains(events.good) and self.isDead:
+    self.regret("death")
+
+if 1 == 0:
+    question.ponder()
